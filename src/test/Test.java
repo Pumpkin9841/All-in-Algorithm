b1f8bba7b69de61a0e37efd2eacdecb9239b2588
fjnuzf <602437361@qq.com>
2022-03-06 20:55:50 +0800

    快排

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,16 +10,45 @@ import java.util.List;
  */
 public class Test {
     public static void main(String[] args) {
-        List<Integer> list = getPer(456123);
-        System.out.println(list.stream().mapToInt(Integer::intValue).sum());
+        Test.quickSort(new int[]{3,1,2,7,7567});
     }
 
-    public static List<Integer> getPer( int num ){
-        ArrayList<Integer> list = new ArrayList<>();
-        while( num > 0 ){
-            list.add(num % 10) ;
-            num /= 10 ;
+    public static void quickSort( int[] array ){
+        int len ;
+        if( array == null
+                || (len = array.length) == 0
+                || len == 1 ){
+            return ;
         }
-        return list ;
+        sort(array , 0 , len-1);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void sort( int[] array , int left , int right ){
+        if( left > right ){
+            return ;
+        }
+        //选一个基准数
+        int base = array[left] ;
+        int i = left , j = right ;
+        while( i != j ){
+            //从右边 找到比基准数小的数
+            while( array[j] >= base && i < j ){
+                j-- ;
+            }
+
+            while( array[i] <= base && i < j ){
+                i++ ;
+            }
+        }
+        if( i < j ){
+            int temp = array[j] ;
+            array[j] = array[i] ;
+            array[i] = temp ;
+        }
+        array[left] = array[i] ;
+        array[i] = base ;
+        sort(array , 0 , i-1);
+        sort(array , i+1 , right);
     }
 }
